265fd18dcc2c593ae410be9a81b62ba4874a983b
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-01-12 15:07:08 -0500

    commit 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div >
+    <BrowserRouter>
+    <div>
       <header >
-       <a href="/">e-commerce</a>
+       <Link to="/">e-commerce</Link>
       </header>
       <main>
-        List products
+        
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:slug" element={<ProductScreen />} />
+        </Routes>
+
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
