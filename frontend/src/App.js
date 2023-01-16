f2d59d559edb15b38c6b4ca3142a3af27956f540
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-01-16 17:04:13 -0500

    commit 6
    create shopping cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,17 @@ import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import {LinkContainer} from 'react-router-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge';
+import { useContext } from 'react';
+import { Store } from './Store';
+import CardScreen from './screens/CartScreen';
 
 
 function App() {
+  const { state } = useContext(Store); 
+  const { cart } = state;
   return (
     <BrowserRouter>
     <div className='d-flex flex-column site-container'>
@@ -16,14 +24,25 @@ function App() {
             <LinkContainer to="/">
               <Navbar.Brand>e-commerce</Navbar.Brand>
             </LinkContainer>
+            <Nav className="me-auto">
+              <Link to="/cart" className="nav-link">
+                Cart
+                {cart.cartItems.length > 0 && (
+                  <Badge pill bg="danger">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </Badge>
+                )}
+                </Link>
+            </Nav>
           </Container>
         </Navbar>
       </header>
       <main>
-        <Container>
+        <Container className='mt-3'>
           <Routes>
-            <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/cart" element={<CardScreen />} />
+            <Route path="/" element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,70 +1,73 @@
-import { useEffect, useReducer, useState } from 'react';
-import { Link } from "react-router-dom";
-import axios from 'axios';
-import logger from 'use-reducer-logger';
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+import logger from "use-reducer-logger";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
 // import data from "../data";
 
 const reducer = (state, action) => {
-    switch(action.type){
-        case 'FETCH_REQUEST':
-            return {...state, loading: true};
-        case 'FETCH_SUCCESS':
-            return{...state, products: action.payload, loading: false};
-        case 'FECTH_FAIL':
-            return {...state, loading: false, error: action.payload};
-        default:
-            return state
-    }
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FECTH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
 };
 
-function HomeScreen(){
-    // const [products, setProducts] = useState([]);
+function HomeScreen() {
+  // const [products, setProducts] = useState([]);
 
-    const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
-        products: [],
-        loading: true,
-        error: '',
-    })
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
 
-    useEffect(()=> {
-        const fetchData = async () => {
-            dispatch({type: 'FETCH_REQUEST'});
-            try {
-                const result = await axios.get('/api/products');
-                dispatch({type: 'FETCH_SUCCESS', payload: result.data})
-            }catch(err){
-                dispatch({type: 'FECTH_FAIL', payload: err.message});
-            }
-            // setProducts(result.data);
-        };
-        fetchData();
-
-    }, []);
-    return ( <div>
-        <h1>Featured products</h1>
-       <div className="products">
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FECTH_FAIL", payload: err.message });
+      }
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Helmet>
+        <title>e-commerce</title>
+      </Helmet>
+      <h1>Featured products</h1>
+      <div className="products">
         {loading ? (
-            <div>Loading...</div>
-          )  : error ? (
-          <div>{error}</div>
-          ) : (
-        
-        products.map(product => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product__info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-                <p><strong>${product.price}</strong></p>
-                <button>Add to cart</button>
-            </div>
-          </div>)))}
-        </div>
+          <LoadingBox/>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
     </div>
-    );
+  );
 }
 
 export default HomeScreen;
