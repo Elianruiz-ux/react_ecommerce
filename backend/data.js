78e11f8b5c43ae772ad201249de423330e839f66
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-01-24 15:59:50 -0500

    commit signin

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data ={
+    users:[
+      {
+        name:'Elian',
+        email: 'admin@admin.com',
+        password: bcrypt.hashSync('123456'),
+        isAdmin: true,
+      },
+      {
+        name:'Joel',
+        email: 'user@user.com',
+        password: bcrypt.hashSync('123456'),
+        isAdmin: false,
+      }
+    ],
     products: [
         {
         //   _id: '1',
