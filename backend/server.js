4bb6478f732e83693747af29aba4985de5b97a1a
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-01-24 09:22:19 -0500

    commit 9
    ya funciona

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import seedRoutes from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("DB CONECTADA");
   })
@@ -15,27 +18,8 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+app.use("/api/seed", seedRoutes);
+app.use("/api/products", productRouter)
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
